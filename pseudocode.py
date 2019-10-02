d3d9b13e4bb24492342084299e5040bc679da211
QX1207 <qian.xiang@rbe.sk.ca>
2019-10-02 09:19:52 -0600

    Second update from Period 1

--- pseudocode.py
+++ pseudocode.py
@@ -23,23 +23,68 @@ else:
     print("Closed")
 
 
-# design the map in my text advanture game
+# print the rules
+
+
+
+# choice a character from 3 character
 if character == "1":
     print("Your ability is...")
-elif choice == "2":
+elif character == "2":
     print("Your ability is...")
 else:
     print("Your ability is...")
+
+# character appear in a random room (empty room)
+
+
+
+# get three choices of common Weapon
+
+
+# show your inventory and weapon bonus
 
 
 # design a map for my whole game
 
 
-# a chest with random weapon
+# design a map for chests
+
+
+# design a map for monsters and bosses
+
+
+# 4 choices to go to other room
+
+
+# find a monster or boss
+
+
+# show your HP and monster(boss)'s HP
+
+
+# 4 choice to decide your reaction, a choice to use your weapon, a choice
+# to use your character ability, and a choice to use your consumble
+
+
+# if the character die, it will have two choice 1. 'encourge you and end
+# the game' or 2. 'refresh and fight with the monster in that rooom again'
+
+
+# kill the monster or boss, you will get a passward or the special key
+
+
+# find a chest and open it
+
+
+# find a random weapon from chest
 import random
 Weapon = ['1', '2']
 print("You found a weapon in a chest.",
       f"You get a common weapon {random.choice(Weapon)}!")
+
+
+# find a random consumble from chest
 
 
 # clues about the boos room's passward
@@ -51,3 +96,19 @@ elif room == "3":
     print("The passward is _ _ 3 _")
 else:
     print("The passward is _ 5 _ _")
+
+
+# make a lock on the door to escape(two way to open the door)
+
+
+# use the special key what did you got from the special chest to open the door
+
+
+# if you did not get the chest, input passward to escape
+
+
+# if passward is incorrect, you can type the passward again or continus your
+# adventure
+
+
+# door was open, you success to escape and end the game

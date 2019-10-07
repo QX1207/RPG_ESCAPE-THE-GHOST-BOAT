0fc2d8919b14351d76d0c591add74685fdf73781
QX1207 <54901364+QX1207@users.noreply.github.com>
2019-10-07 08:59:21 -0600

    Update weapon.py

---
+++ weapon.py
@@ -0,0 +1,14 @@
+# a list of common weapon in the game
+commonWeapon = ['Wood Sword', 'Stone Sword', 'Bone Knife', 'Stone Knife',
+                'Wood Knife', 'Stone Hatchet', 'Wood Dragger', 'Stone Dragger'
+                'Broken Bow and Arrow - Dragon and Triger',
+                'Wood Bow and Arrow']
+# a list of rare weapon in the game
+rareWeapon = ["Ice Wand - Frozen Ghost", "Wind Sword - Death Zone",
+              "Fire Dagger - Fire Punishment", "Dark Dragger - Infinite",
+              "Mechanical Umbrella - Thousands of Organ"]
+# a list of epic weapon in the game
+epicWeapon = ["Star Wand - Final Justice", "Death Stone - Dark Hallow",
+              "Unknown Sword - Broken Sword from the Stone"]
+# a list of legenary weapon in the game
+legendaryWeapon = "Chaos Mirror"

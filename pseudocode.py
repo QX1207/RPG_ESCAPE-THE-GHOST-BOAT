cc7cd6a97b9aae25ba01be455831a3852e6d5062
QX1207 <qian.xiang@rbe.sk.ca>
2019-10-11 09:11:53 -0600

    Update pseudocode.py

--- pseudocode.py
+++ pseudocode.py
@@ -7,21 +7,48 @@
 
 
 # print a statement to welcome player
+# ("Welcome to the Ghost Ship - SV Mary Celeste.
+#   Everything isn't real, except you.")
 
 
 # input the player name
 
 
-# a selection before player enter the game
+# a menu before player enter the game
+# use if statement to print three selections
+# start - enter the beginning of the game
+# inventory introduction: print out the base information about the weapon
+# consumble... (maybe change on after version)
+# rules - print out the informations about the game
+# end -stop tp run the code
 
 
 # print the rules
+# - There are level c, b monsters in normal rooms. You will get common weapon,
+# rare weapon, and consumble
+# empty room might have chest with rare weapon, epic weapon, and consumble
+# - There are 2 level 2 bosses in special rooms. One with special key.
+# The other with legendary weapon.
+# (Coming soon...)
 
 
 # choice a character from 3 character
+# character files:
+# Character number 1
+#   name: doctor Jame Dyer
+#   ability: Med Master - you can choose to heal 50 HP
+#            when you are fighting with boss
 
 
-# character appear in a random room (empty room)
+#   name: Thief - Darius Pierson
+#   ability: Hoarder - you will get one more weapon after you kill a boss
+
+
+#   name: Soldier - Naib Collins
+#   ability: Skilled - before you enter a new rooom, you can change
+#            a random weapon
+
+# character appear in the room 1 (empty room)
 
 
 # get three choices of common Weapon
@@ -31,7 +58,12 @@
 
 
 # design a map for my whole game
-
+#                       North
+#                |          |        |S level BOSS|
+#       Start    |          |        |            |
+#                |          |        |            |
+#                |          |        |   Escape   |
+#                       South                      
 
 # design a map for chests
 
@@ -68,10 +100,10 @@
 # find a random consumble from chest
 
 
-# clues about the boos room's passward
+# you will get clues about the escape room's passward after you open the chest
 
 
-# make a lock on the door to escape(two way to open the door)
+# find a lock on the door in the escaoe room(two way to open the door)
 
 
 # use the special key what did you got from the special chest to open the door

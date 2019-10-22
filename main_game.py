914e6b2bad965f46808ff6963f99c2d92873acd6
QX1207 <qian.xiang@rbe.sk.ca>
2019-10-22 09:19:55 -0600

    Update main_game.py

--- main_game.py
+++ main_game.py
@@ -15,32 +15,38 @@
 #            a random weapon
 
 
+
 characters = {
     'Doctor': {
         'firstName': 'Jame',
         'lastName': 'Dyer',
-        'ability': 'Med Master - you can choose to heal 50 HP when you are',
-        ' fighting with boss',
+        'ability': 'Med Master - you can choose to heal 50 HP when you are'
+        'fighting with boss',
+        'health': '100'
         },
     'Thief': {
         'firstName': 'Darius',
         'lastName': 'Pierson',
-        'ability': 'Hoarder - you will get one more weapon after you',
+        'ability': 'Hoarder - you will get one more weapon after you'
         'kill a boss',
+        'health': '120',
         },
     'Soldier': {
         'firstName': 'Naib',
         'lastName': 'Collins',
-        'ability': 'Skilled - before you enter a new rooom, you can change',
-        'a random weapon',
-        }, 
+        'ability': 'Skilled - before you enter a new rooom, you can change a'
+        'random weapon',
+        'health': '200',
+        },
 
-}
+        }
 
 
 for characters, characters_info in characters.items():
-v print(f"\nCharacters: {characters}")
-w full_name = f"{characters_info['firstName']} {characters_info['lastName']}"
-location = characters_info['ability']
-x print(f"\tFull name: {full_name.title()}")
-print(f"\tLocation: {ability.title()}")
+    print(f"\nCharacters: {characters}")
+    full_name = f"{characters_info['firstName']} {characters_info['lastName']}"
+    ability = characters_info['ability']
+    health = characters_info["health"]
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tAblility: {ability.title()}")
+    print(f"\t{characters} has {health} health point")

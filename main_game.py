4c0f07be796c31a7960ed2a6e69ab4c90b8705ca
QX1207 <qian.xiang@rbe.sk.ca>
2019-10-23 09:24:41 -0600

    Update main_game.py

--- main_game.py
+++ main_game.py
@@ -21,25 +21,28 @@ characters = {
         'firstName': 'Jame',
         'lastName': 'Dyer',
         'ability': 'Med Master - you can choose to heal 50 HP when you are'
-        'fighting with boss',
-        'health': '100'
+        ' fighting with boss',
+        'health': '1500',
+        'damage': '170',
         },
     'Thief': {
         'firstName': 'Darius',
         'lastName': 'Pierson',
         'ability': 'Hoarder - you will get one more weapon after you'
-        'kill a boss',
-        'health': '120',
+        ' kill a boss',
+        'health': '1200',
+        'damage': '250',
         },
     'Soldier': {
         'firstName': 'Naib',
         'lastName': 'Collins',
         'ability': 'Skilled - before you enter a new rooom, you can change a'
-        'random weapon',
-        'health': '200',
+        ' random weapon',
+        'health': '2000',
+        'damage': '200',
         },
 
-        }
+    }
 
 
 for characters, characters_info in characters.items():
@@ -47,6 +50,47 @@ for characters, characters_info in characters.items():
     full_name = f"{characters_info['firstName']} {characters_info['lastName']}"
     ability = characters_info['ability']
     health = characters_info["health"]
+    damage = characters_info["damage"]
     print(f"\tFull name: {full_name.title()}")
-    print(f"\tAblility: {ability.title()}")
+    print(f"\tAblility: {ability}")
     print(f"\t{characters} has {health} health point")
+    print(f"\tBasic Attack Damage: {damage}")
+
+
+# commonWeapon = ['Wood Sword', 'Stone Sword', 'Bone Knife', 'Stone Knife',
+#                'Wood Knife', 'Stone Hatchet', 'Wood Dragger', 'Stone Dragger'
+#                'Broken Bow and Arrow - Dragon and Triger',
+#                'Wood Bow and Arrow']
+# a list of rare weapon in the game
+# rareWeapon = ["Ice Wand - Frozen Ghost", "Wind Sword - Death Zone",
+#              "Fire Dagger - Fire Punishment", "Dark Dragger - Infinite",
+#              "Mechanical Umbrella - Thousands of Organ"]
+# a list of epic weapon in the game
+# epicWeapon = ["Star Wand - Final Justice", "Death Stone - Dark Hallow",
+#              "Unknown Sword - Broken Sword from the Stone"]
+# a list of legenary weapon in the game
+# legendaryWeapon = "Chaos Mirror"
+
+
+weaponInventory = {'Wood Sword': {'bonus attack damage': '',
+                   'description': ''},
+                   'Stone Sword': {'bonus attack damage': '',
+                   },
+                   'Stone Knife': {'bonus attack damage': '',
+                   },
+                   'Wood Knife': {'bonus attack damage': '',
+                   },
+                   'Stone Sword': {'bonus attack damage': '',
+                   },
+                   'Stone Hatchet': {'bonus attack damage': '',
+                   },
+                   'Wood Dragger': {'bonus attack damage': '',
+                   },
+                   'Stone Dragger': {'bonus attack damage': ',
+                   '},
+                   'Broken Bow and Arrow - Dragon and Triger':
+                   {'bonus attack damage': '',
+                   },
+                   'Wood Bow and Arrow': {'bonus attack damage': '',
+                   }
+                   }

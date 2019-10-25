6bed48773e803b497dbfcf0d110054d4d86851ee
QX1207 <54901364+QX1207@users.noreply.github.com>
2019-10-25 01:17:17 -0600

    Updated #1
    It is a referee for my game. More weapons, locations, and characters are coming.

--- main_game.py
+++ main_game.py
@@ -1,52 +1,45 @@
-# choice a character from 3 character
-# character files:
-# Character number 1
-#   name: doctor Jame Dyer
-#   ability: Med Master - you can choose to heal 50 HP
-#            when you are fighting with boss
+# Basic information about updated #1 in the mian_game.py
+# Edited person: Qian Xiang
+# Last updated date: 10/25/2019
 
 
-#   name: Thief - Darius Pierson
-#   ability: Hoarder - you will get one more weapon after you kill a boss
-
-
-#   name: Soldier - Naib Collins
-#   ability: Skilled - before you enter a new rooom, you can change
-#            a random weapon
-
-
-
+# create a nested dictionary (a dictionary in a dictionary) for the characters
 characters = {
     'Doctor': {
+        'number': '#1',
         'firstName': 'Jame',
         'lastName': 'Dyer',
         'ability': 'Med Master - you can choose to heal 50 HP when you are'
         ' fighting with boss',
         'health': '1500',
         'damage': '170',
-        },
+    },
     'Thief': {
+        'number': '#2',
         'firstName': 'Darius',
         'lastName': 'Pierson',
         'ability': 'Hoarder - you will get one more weapon after you'
         ' kill a boss',
         'health': '1200',
         'damage': '250',
-        },
+    },
     'Soldier': {
+        'number': '#3',
         'firstName': 'Naib',
         'lastName': 'Collins',
         'ability': 'Skilled - before you enter a new rooom, you can change a'
         ' random weapon',
         'health': '2000',
         'damage': '200',
-        },
+    },
 
-    }
+}
 
 
+# use for-loop to print out my statement about characters line by line
 for characters, characters_info in characters.items():
-    print(f"\nCharacters: {characters}")
+    number = characters_info["number"]
+    print(f"\nCharacters {number}: {characters}")
     full_name = f"{characters_info['firstName']} {characters_info['lastName']}"
     ability = characters_info['ability']
     health = characters_info["health"]
@@ -57,40 +50,66 @@ for characters, characters_info in characters.items():
     print(f"\tBasic Attack Damage: {damage}")
 
 
-# commonWeapon = ['Wood Sword', 'Stone Sword', 'Bone Knife', 'Stone Knife',
-#                'Wood Knife', 'Stone Hatchet', 'Wood Dragger', 'Stone Dragger'
-#                'Broken Bow and Arrow - Dragon and Triger',
-#                'Wood Bow and Arrow']
-# a list of rare weapon in the game
-# rareWeapon = ["Ice Wand - Frozen Ghost", "Wind Sword - Death Zone",
-#              "Fire Dagger - Fire Punishment", "Dark Dragger - Infinite",
-#              "Mechanical Umbrella - Thousands of Organ"]
-# a list of epic weapon in the game
-# epicWeapon = ["Star Wand - Final Justice", "Death Stone - Dark Hallow",
-#              "Unknown Sword - Broken Sword from the Stone"]
-# a list of legenary weapon in the game
-# legendaryWeapon = "Chaos Mirror"
+# print two empty line that make it easy to read
+print("\n")
 
 
-weaponInventory = {'Wood Sword': {'bonus attack damage': '',
-                   'description': ''},
-                   'Stone Sword': {'bonus attack damage': '',
-                   },
-                   'Stone Knife': {'bonus attack damage': '',
-                   },
-                   'Wood Knife': {'bonus attack damage': '',
-                   },
-                   'Stone Sword': {'bonus attack damage': '',
-                   },
-                   'Stone Hatchet': {'bonus attack damage': '',
-                   },
-                   'Wood Dragger': {'bonus attack damage': '',
-                   },
-                   'Stone Dragger': {'bonus attack damage': ',
-                   '},
-                   'Broken Bow and Arrow - Dragon and Triger':
-                   {'bonus attack damage': '',
-                   },
-                   'Wood Bow and Arrow': {'bonus attack damage': '',
-                   }
-                   }
+# create a nested dictionary (a dictionary in a dictionary) for the inventories
+weaponInventory = {
+    'Wood Sword': {
+                  'bonusAttackDamage': '25',
+                  'description': 'A common sword made by wood',
+                  'attackRanges': '5m',
+                  'attackSpeed': '15%'
+    },
+    'Stone Sword': {
+                   'bonusAttackDamage': '40',
+                   'description': 'A common sword made by stone',
+                   'attackRanges': '5m',
+                   'attackSpeed': '10%'
+    },
+    'Stone Knife': {
+                   'bonusAttackDamage': '30',
+                   'description': 'A common knife made by stone',
+                   'attackRanges': '60',
+                   'attackSpeed': '10%'
+    }
+}
+
+
+# print out the name of this inventory
+print(f"\nInventory(common weapon):")
+
+
+# use for-loop to print out my statement about inventories line by line
+for weaponInventory, weaponInventory_info in weaponInventory.items():
+    print(f"\n*{weaponInventory}")
+    bonusAttackDamage = weaponInventory_info['bonusAttackDamage']
+    description = weaponInventory_info["description"]
+    attackRanges = weaponInventory_info["attackRanges"]
+    attackSpeed = weaponInventory_info["attackSpeed"]
+    print(f"\tBonus Attack Damage: {bonusAttackDamage}")
+    print(f"\tDescription: {description}")
+    print(f"\tAttack Range: {attackRanges}")
+    print(f"\tAttack Speed: {attackSpeed}")
+
+
+# print two empty line that make it easy to read
+print("\n")
+
+
+# create a nested dictionary (a dictionary in a dictionary) for the locations
+currentLocations = {
+    'room #1': {'l1': 'Wheelhouse', 'l2': 'Level C Boss'},
+    'room #2': {'l1': 'Accommodators', 'l2': 'Level C Boss'},
+    'room #3': {'l1': 'Mess Area', 'l2': 'Level B Boss'},
+    'room #4': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
+}
+
+
+# use for-loop to print out my statement about locations line by line
+for name, currentlocations_info in currentLocations.items():
+    l1 = currentlocations_info["l1"]
+    l2 = currentlocations_info["l2"]
+    print(f"\n{name.title()} is {l1} on the ship. There is"
+          f" a {l2} in this area")

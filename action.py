a2b2ff1dbd3928d8f31edec9a8334cb440bf95db
QX1207 <54901364+QX1207@users.noreply.github.com>
2019-10-29 23:37:53 -0600

    Update #2 RPG action menu(finished)
    It is a menu made by input and if statement.

--- action.py
+++ action.py
@@ -1,11 +1,18 @@
-# a list for directions
-directions = ['North', 'East', 'South', 'West']
-# use for loop to print the list directions one by one
-for direction in directions:
-    print(direction)
-# a statement to explain the choices what do player have
-move = "Go in one of the following direction: "
-# create a user input 
-name = input(move)
-# print the statement that explain the characters' action
-print(f"\nGo {name}.")
+# print all directions what can player choose
+print("1. North")
+print("2. East")
+print("3. South")
+print("4. West")
+# print out a statement that introduce the choices they face
+print("Go in one of the direction above (please enter the number):")
+# create a user input
+questions = int(input("Enter choice: "))
+# use if-elif-else statement to explain different movements
+if selection == 1:
+    print("You are going forward to the next room.")
+elif selection == 2:
+    print("You are going to the right room.")
+elif selection == 3:
+    print("You are going backward.")
+else:
+    print("You are going to the left room.")

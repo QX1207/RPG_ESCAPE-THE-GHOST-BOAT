4e018064ad8c77ec0ee1d1d57df4b5aef33bf627
QX1207 <qian.xiang@rbe.sk.ca>
2019-10-30 09:31:19 -0600

    Update #3 A new menu about action
    It is a new menu with dictionary, random, and...

--- action.py
+++ action.py
@@ -1,3 +1,5 @@
+# imnport random
+import random
 # print all directions what can player choose
 print("1. North")
 print("2. East")
@@ -6,13 +8,49 @@ print("4. West")
 # print out a statement that introduce the choices they face
 print("Go in one of the direction above (please enter the number):")
 # create a user input
-questions = int(input("Enter choice: "))
+selections = int(input("Enter choice: "))
 # use if-elif-else statement to explain different movements
-if selection == 1:
+if selections == 1:
     print("You are going forward to the next room.")
-elif selection == 2:
+elif selections == 2:
     print("You are going to the right room.")
-elif selection == 3:
+elif selections == 3:
     print("You are going backward.")
 else:
     print("You are going to the left room.")
+
+
+# 7 choice to decide your reaction, attack(make damage to the boss),
+# dodge(dodge the boss's attack and make a few damage to the boss, maybe fail),
+# defense(defense a part a damage from the boss), run back(),
+# a choice to use your weapon, a choice to use your character ability
+# (only doctor yet), and a choice to use your consumble
+# print a multiple choise question about the action
+print("Choose the following action to beat the boss",
+      "(please select the number): ")
+# create a list about the character's actions when you meet a bosses
+characterActions = ['attack', 'dodge', 'defense', 'run back',
+                    'use your ability', 'use your weapon',
+                    'use your consumble']
+# use for loop to print my selections
+for characterAction in characterActions:
+    print(f"{characterActions.index(characterAction) + 1}, {characterAction}")
+# create a user input
+actions = int(input("You choose to "))
+# use if-elif-else statement tp explain different acton from you character
+if actions == 1:
+    print("You make damage to the boss.")
+elif actions == 2:
+    dodge = ("You dodge the boss's attack and make a few damage to the boss.",
+             "Boss speed is very fast. You dodged that fail.")
+    print(random.choice(dodge))
+elif actions == 3:
+    print("You defense a part a damage from the boss.")
+elif actions == 4:
+    print("You run back to the last room.")
+elif actions == 5:
+    print("Ablility list")
+elif actions == 6:
+    print("Weapon Inventory")
+else:
+    print("Consumble Inventory")

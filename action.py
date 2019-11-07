dd079b630c4f5de01de0ddec1d2b137f5f2d3ee5
QX1207 <qian.xiang@rbe.sk.ca>
2019-11-07 09:29:15 -0600

    Continuous Game Play Update #1
    The action.py file was insert a functions' defnition. There are not descriptions about the code yet. There is a bug. The input value cannot be any words or letters because of the int.

--- action.py
+++ action.py
@@ -1,23 +1,33 @@
-# imnport random
+## imnport random
 import random
+def directions(selections):
 # print all directions what can player choose
-print("1. North")
-print("2. East")
-print("3. South")
-print("4. West")
+    print("1. North")
+    print("2. East")
+    print("3. South")
+    print("4. West")
 # print out a statement that introduce the choices they face
 print("Go in one of the direction above (please enter the number):")
 # create a user input
-selections = int(input("Enter choice: "))
+while True:
+    directions(selections)
+    selections = int(input("Enter choice: "))
 # use if-elif-else statement to explain different movements
-if selections == 1:
-    print("You are going forward to the next room.")
-elif selections == 2:
-    print("You are going to the right room.")
-elif selections == 3:
-    print("You are going backward.")
-else:
-    print("You are going to the left room.")
+    if selections == 1:
+        print("You are going forward to the next room.")
+        print("\n")
+    elif selections == 2:
+        print("You are going to the right room.")
+        print("\n")
+    elif selections == 3:
+        print("You are going backward.")
+        print("\n")
+    elif selections == 4:
+        print("You are going to the left room. You find the monster!")
+        break
+    else:
+        print("Please enter the following number of directions to move")
+        print("\n")
 
 
 # 7 choice to decide your reaction, attack(make damage to the boss),
@@ -32,25 +42,37 @@ print("Choose the following action to beat the boss",
 characterActions = ['attack', 'dodge', 'defense', 'run back',
                     'use your ability', 'use your weapon',
                     'use your consumble']
+def behavior(characterActions):
 # use for loop to print my selections
-for characterAction in characterActions:
-    print(f"{characterActions.index(characterAction) + 1}, {characterAction}")
-# create a user input
-actions = int(input("You choose to "))
+    for characterAction in characterActions:
+        print(f"{characterActions.index(characterAction) + 1}. {characterAction}")
+while True:
+    behavior(characterActions)
+    # create a user input
+    actions = int(input("You choose to "))
 # use if-elif-else statement to explain different action from you character
-if actions == 1:
-    print("You make damage to the boss.")
-elif actions == 2:
-    dodge = ("You dodge the boss's attack and make a few damage to the boss.",
+    if actions == 1:
+        print("You make damage to the boss.")
+        print("\n")
+    elif actions == 2:
+        dodge = ("You dodge the boss's attack and make a few damage to the boss.",
              "Boss speed is very fast. You dodged that fail.")
-    print(random.choice(dodge))
-elif actions == 3:
-    print("You defense a part a damage from the boss.")
-elif actions == 4:
-    print("You run back to the last room.")
-elif actions == 5:
-    print("Ablility list")
-elif actions == 6:
-    print("Weapon Inventory")
-else:
-    print("Consumble Inventory")
+        print(random.choice(dodge))
+        print("\n")
+    elif actions == 3:
+        print("You defense a part a damage from the boss.")
+        print("\n")
+    elif actions == 4:
+        print("You run back to the last room.")
+        break
+    elif actions == 5:
+        print("Ablility list")
+        print("\n")
+    elif actions == 6:
+        print("Weapon Inventory")
+        print("\n")
+    elif actions == 7:
+        print("Consumble Inventory")
+        print("\n")
+    else:
+        print("Please enter a number to finish your action")

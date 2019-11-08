b7eb1fece8ecca24e10b61721d1a2a2c43d26950
QX1207 <qian.xiang@rbe.sk.ca>
2019-11-08 09:31:45 -0600

    Final Version
    All bug was fixed.
    Changelog was updated.

--- action.py
+++ action.py
@@ -1,17 +1,23 @@
 # imnport random
 import random
+# print out a statement about the story
+print("There are some monsters on the ship. Let us begin the adventure.")
+# create a llist about the directions
+playerDirections = ["North", "East", "South", "West(quit)"]
+
+
 # create a definition of the function
-def directions(selections):
-# print all directions what can player choose
-    print("1. North")
-    print("2. East")
-    print("3. South")
-    print("4. West")
+# use for loop to print my selections
+def directions(playerDirections):
+    for playerDirection in playerDirections:
+        print(
+            f"{playerDirections.index(playerDirection) + 1}.{playerDirection}"
+            )
 # print out a statement that introduce the choices they face
-print("Go in one of the direction above (please enter the number):")
-# use while loop to repeat the question
+    print("Go in one of the direction above (please enter the number):")
+# after for loop, use while loop to repeat the question until it is break
 while True:
-    directions(selections)
+    directions(playerDirections)
 # create a user input
     selections = int(input("Enter choice: "))
 # use if-elif-else statement to explain different movements
@@ -26,6 +32,7 @@ while True:
         print("\n")
     elif selections == 4:
         print("You are going to the left room. You find the monster!")
+        print("\n")
         break
     else:
         print("Please enter the following number of directions to move")
@@ -37,19 +44,24 @@ while True:
 # defense(defense a part a damage from the boss), run back(),
 # a choice to use your weapon, a choice to use your character ability
 # (only doctor yet), and a choice to use your consumble
+
+
 # print a multiple choise question about the action
 print("Choose the following action to beat the boss",
       "(please enter the number): ")
 # create a list about the character's actions when you meet a bosses
-characterActions = ['attack', 'dodge', 'defense', 'run back',
+characterActions = ['attack', 'dodge', 'defense', 'run back(quit)',
                     'use your ability', 'use your weapon',
                     'use your consumble']
+
+
 # create a definition of the function
-def behavior(characterActions):
 # use for loop to print my selections
+def behavior(characterActions):
     for characterAction in characterActions:
-        print(f"{characterActions.index(characterAction) + 1}.
-                {characterAction}")
+        print(
+            f"{characterActions.index(characterAction) + 1}. {characterAction}"
+            )
 # after for loop, use while loop to repeat the question until it is break
 while True:
     behavior(characterActions)
@@ -60,8 +72,9 @@ while True:
         print("You make damage to the boss.")
         print("\n")
     elif actions == 2:
-        dodge = ("You dodge the boss's attack and make a few damage to the boss.",
-             "Boss speed is very fast. You dodged that fail.")
+        dodge = ("You dodge the boss's attack and make a few damage to"
+                 " the boss.",
+                 "Boss speed is very fast. You dodged that fail.")
         print(random.choice(dodge))
         print("\n")
     elif actions == 3:
@@ -69,6 +82,7 @@ while True:
         print("\n")
     elif actions == 4:
         print("You run back to the last room.")
+        print("\n")
         break
     elif actions == 5:
         print("Ablility list")
@@ -81,3 +95,4 @@ while True:
         print("\n")
     else:
         print("Please enter a number to finish your action")
+        print("\n")

55ffe48b7c98fe70226823c48efae7c30db9d491
QX1207 <54901364+QX1207@users.noreply.github.com>
2019-11-17 20:05:11 -0600

    Working...

--- map.py
+++ map.py
@@ -7,4 +7,34 @@ import random
 from tabulate import tabulate
 
 
-def
+currentLocations = {
+    'room #1': {'l1': 'Wheelhouse', 'l2': 'Level C Boss'},
+    'room #2': {'l1': 'Accommodators', 'l2': 'Level C Boss'},
+    'room #3': {'l1': 'Mess Area', 'l2': 'Level B Boss'},
+    'room #4': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
+    'room #5': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
+    'room #6': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
+    'room #7': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
+    'room #8': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
+    'room #9': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
+    'room #10': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
+    'room #11': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
+    'room #12': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
+    'room #13': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
+    'room #14': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
+    'room #15': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
+    'room #16': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
+}
+
+
+def print_map():
+        """Print a map of the ship with directions"""
+        ship_printable = """
+                            North
+                |          |Supplies|            |
+        port    |          |        |You are Here|  starboard
+                |          | Start  |            |
+                |Escape Pod|        |            |
+                        South
+        """
+        print(ship_printable)

e18f72eb69c4c030c0ad6a0f3fecbf9d4b22dfdb
QX1207 <54901364+QX1207@users.noreply.github.com>
2019-11-18 20:04:15 -0600

    Updated #1 map
    working...

--- map.py
+++ map.py
@@ -8,33 +8,33 @@ from tabulate import tabulate
 
 
 currentLocations = {
-    'room #1': {'l1': 'Wheelhouse', 'l2': 'Level C Boss'},
-    'room #2': {'l1': 'Accommodators', 'l2': 'Level C Boss'},
-    'room #3': {'l1': 'Mess Area', 'l2': 'Level B Boss'},
-    'room #4': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
-    'room #5': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
-    'room #6': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
-    'room #7': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
-    'room #8': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
-    'room #9': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
-    'room #10': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
-    'room #11': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
-    'room #12': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
-    'room #13': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
-    'room #14': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
-    'room #15': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
-    'room #16': {'l1': 'Preparation room', 'l2': 'Level A Boss'},
+    "room #1": "Officer's Quarters",
+    "room #2": "Navgation",
+    "room #3": "First Mate's Quarters",
+    "room #4": "Captain's Quarters",
+    "room #5": "Fore Castle Deck",
+    "room #6": "Long Boat (Escape gate)",
+    "room #7": "Main-Mast",
+    "room #8": "Wheel",
+    "room #9": "Fore Hold",
+    "room #10": "Cargo Access",
+    "room #11": "Capstan",
+    "room #12": "Officer's Mess",
+    "room #13": "Live stock Hold",
+    "room #14": "Infirmary",
+    "room #15": "Cable Stores",
+    "room #16": "Carpenter Stores"
 }
 
 
 def print_map():
-        """Print a map of the ship with directions"""
+        """Print a map of the ghost ship with directions"""
         ship_printable = """
                             North
-                |          |Supplies|            |
-        port    |          |        |You are Here|  starboard
-                |          | Start  |            |
-                |Escape Pod|        |            |
-                        South
+                |          |        |            |
+                |          |        |            |
+                |          |        |            |
+                |          |        |            |
+                            South
         """
         print(ship_printable)

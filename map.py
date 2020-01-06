e5a017658fab81e15171111231b732d197297adb
QX1207 <qian.xiang@rbe.sk.ca>
2020-01-06 08:55:42 -0600

    Character Information
    Renewed the class CharacterI that store the beginning point of the character and some informations about inventory...

--- map.py
+++ map.py
@@ -19,8 +19,9 @@ def main_map():
         print(room)
 
 
-# definite the print_map and print it for player
-def print_map():
+# definite the print_map and print it for player in a class
+class visual_map:
+    def print_map():
         """Print a map of the ghost ship with directions"""
         print_map = """
                                 North
@@ -29,28 +30,34 @@ def print_map():
     | Fore Hold        |Cargo Access|     Capstan         |  Officer's Mess  |
     | Live stock Hold  |Infirmary   |     Cable Stores    | Carpenter Stores |
                                 South
-        """
+    """
         print(print_map)
 
 
 main_map()
-print_map()
+visual_map()
 
 
 class CharactersI():
     """CharactersI class with characters' informations"""
-    def__init__(self):
+    def __init__(self, x, y):
         # Player starting coordinates
-        self.x =
-        self.y =
+        self.x = x
+        self.y = y
         """initialize attributes of the parent class"""
         self.hp = 1000
         self.victory =False
-
 
     def is_alive(self):
         """player is alive if they have at least 1 HP"""
         return self.hp > 0
 
+    def __str__(self):
+        return self.inventory
 
-    def 
+    def intro_text(self):
+        raise NotImplementedError("Create a subclass instead!")
+
+    def modify_player(self, player):
+        """Added modify_player to every tile"""
+        pass

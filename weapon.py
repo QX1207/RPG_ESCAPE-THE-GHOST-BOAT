702666b4dbe851ec12986c14c3b0d3484b829668
QX1207 <qian.xiang@rbe.sk.ca>
2020-01-15 13:05:45 -0600

    renew the weapon inventory
    updated the dictionary of the weapon in weapon.py

--- weapon.py
+++ weapon.py
@@ -1,27 +1,84 @@
 # just import random to give a random weapon at the beginning of the game
 import random
 # a dictionary of weapon in the game
-weapon = {"Common Weapon":{'Wood Sword':
-                            {}
-                            },
-                'Stone Sword',
-                'Bone Knife',
-                'Stone Knife',
-                'Wood Knife',
-                'Stone Hatchet',
-                'Wood Dragger',
-                'Stone Dragger'
-                'Broken Bow and Arrow - Dragon and Triger',
-                'Wood Bow and Arrow'},
-            {"Rare Weapon": {"Ice Wand - Frozen Ghost",
-                "Wind Sword - Death Zone",
-                "Fire Dagger - Fire Punishment",
-                "Dark Dragger - Infinite",
-                "Mechanical Umbrella - Thousands of Organ"},
-            {"epicWeapon":{"Star Wand - Final Justice",
-                "Death Stone - Dark Hallow",
-                "Unknown Sword - Broken Sword from the Stone"},
-            {"Legendar yWeapon": {"Chaos Mirror"}
+weapon = {"Common Weapon": {'Wood Sword':
+                           {'description': "",
+                            'damage': 10,
+                            'protection':0 },
+                           'Stone Sword':
+                           {'description': "",
+                            'damage': 0,
+                            'protection': 0},
+                           'Bone Knife':
+                           {'description': "",
+                            'damage': 0,
+                            'protection': 0},
+                           'Stone Knife':
+                           {'description': "",
+                            'damage': 0,
+                            'protection': 0},
+                           'Wood Knife':
+                           {'description': "",
+                            'damage': 0,
+                            'protection': 0},
+                           'Stone Hatchet':
+                           {'description': "",
+                            'damage': 0,
+                            'protection': 0},
+                           'Wood Dragger':
+                           {'description': "",
+                            'damage': 0,
+                            'protection': 0},
+                           'Stone Dragger':
+                           {'description': "",
+                            'damage': 0,
+                            'protection': 0},
+                           'Broken Bow and Arrow - Dragon and Triger':
+                           {'description': "",
+                            'damage': 0,
+                            'protection': 0},
+                           'Wood Bow and Arrow':
+                           {'description': "",
+                            'damage': 0,
+                            'protection': 0}},
+        "Rare Weapon": {"Ice Wand - Frozen Ghost":
+                        {'description': "",
+                         'damage': 0,
+                         'protection': 0},
+                        "Wind Sword - Death Zone":
+                        {'description': "",
+                         'damage': 0,
+                         'protection': 0},
+                        "Fire Dagger - Fire Punishment":
+                        {'description': "",
+                         'damage': 0,
+                         'protection': 0},
+                        "Dark Dragger - Infinite":
+                        {'description': "",
+                         'damage': 0,
+                         'protection': 0},
+                        "Mechanical Umbrella - Thousands of Organ":
+                        {'description': "",
+                         'damage': 0,
+                         'protection': 0},
+                        },
+        "epicWeapon": {"Star Wand - Final Justice":
+                       {'description': "",
+                        'damage': 0,
+                        'protection': 0},
+                       "Death Stone - Dark Hallow":
+                       {'description': "",
+                        'damage': 0,
+                        'protection': 0},
+                       "Unknown Sword - Broken Sword from the Stone":
+                       {'description': "",
+                        'damage': 0,
+                        'protection': 0},
+        "Legendar yWeapon": {"Chaos Mirror":
+                             {'description': "",
+                              'damage': 0,
+                              'protection': 0}
+        }
 
 
 def player_weapon(player, weapon):
